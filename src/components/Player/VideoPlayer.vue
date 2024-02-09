118388423c1c9df2f01f1390c7aa7796465ad4c4
Guillermo Eyherabide <eyherabideg@gmail.com>
2024-02-09 17:50:01 -0300

    add

--- src/components/Player/VideoPlayer.vue
+++ src/components/Player/VideoPlayer.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="video-player">
-    <video v-media-ref="setPlayer" :src="props.src" width="640" height="360">
-      Your browser does not support the video tag.
-    </video>
-    <div class="flex align-middle justify-center">
+  <div class="video-player p-6 rounded-lg shadow-lg overflow-hidden relative">
+    <video
+      v-media-ref="setPlayer"
+      :src="props.src"
+      class="w-full rounded-lg neon-border"
+      controls
+    ></video>
+    <div class="flex align-middle justify-center mt-4">
       <slot name="controls">
-        <button
-          class="bg-red-500 text-white text-lg my-4 p-4 w-full rounded-md"
-          @click="handlePlay"
-        >
+        <button class="btn-neon" @click="handlePlay">
           {{ isPlaying ? 'Pause' : 'Play' }} video
         </button>
       </slot>
@@ -41,3 +41,32 @@ const handlePlay = () => {
   props.togglePlay()
 }
 </script>
+<style>
+.neon-border {
+  box-shadow:
+    0 0 5px rgba(255, 0, 255, 0.5),
+    0 0 15px rgba(255, 0, 255, 0.5),
+    0 0 30px rgba(255, 0, 255, 0.7);
+}
+.btn-neon {
+  background: #111;
+  color: #fff;
+  text-shadow:
+    0 0 5px #e60073,
+    0 0 15px #e60073,
+    0 0 20px #e60073,
+    0 0 40px #e60073;
+  padding: 1rem 2rem;
+  border: none;
+  cursor: pointer;
+  border-radius: 8px;
+  font-size: 1rem;
+}
+.btn-neon:hover {
+  box-shadow:
+    0 0 5px #e60073,
+    0 0 15px #e60073,
+    0 0 20px #e60073,
+    0 0 40px #e60073;
+}
+</style>
